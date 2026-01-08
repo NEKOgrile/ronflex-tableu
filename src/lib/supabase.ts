0b08310aba022f8188732c58e57c849e60351c93
NEKOgrile <willemcornil@gmail.com>
2026-01-08 21:58:55 +0100

    update

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -1,6 +1,11 @@
 import { createClient } from "@supabase/supabase-js";
 
-const supabaseUrl = import.meta.env.VITE_SUPABASE_URL || "";
-const supabaseKey = import.meta.env.VITE_SUPABASE_ANON_KEY || "";
+const supabaseUrl =
+  import.meta.env.VITE_SUPABASE_URL ||
+  "https://pwlccqqmqptodugmjvxc.supabase.co";
+
+const supabaseKey =
+  import.meta.env.VITE_SUPABASE_ANON_KEY ||
+  "sb_publishable_jIeb325OrodW07DXGitK9w_OJGSke_F";
 
 export const supabase = createClient(supabaseUrl, supabaseKey);
